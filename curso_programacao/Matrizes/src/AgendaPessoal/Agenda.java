58ad2ffff4f14d82b3ba18f3671ae39f95ae3bcb
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2025-08-31 12:19:50 -0300

    praticando
    ajuste feito no programa para ler  não so o dia e o horario, mas também o mes.

--- curso_programacao/Matrizes/src/AgendaPessoal/Agenda.java
+++ curso_programacao/Matrizes/src/AgendaPessoal/Agenda.java
@@ -4,91 +4,104 @@ import java.util.Scanner;
 
 public class Agenda {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-            String [] [] compromissos = new String[31][24];
+        String[][][] compromissos = new String[12][31][24];
+        // Mês, Dias, Horas
 
-            boolean sair = false;
-            byte opcao;
-            while (!sair){
-                System.out.println("Escolha a opção: \n" +
-                        " 1 - Adicionar compromisso \n" +
-                        " 2 - Verifciar compromisso \n" +
-                        " 3 - Sair");
-                opcao = sc.nextByte();
+        boolean sair = false;
+        byte opcao;
+        while (!sair) {
+            System.out.println("Escolha a opção: \n" +
+                    " 1 - Adicionar compromisso \n" +
+                    " 2 - Verifciar compromisso \n" +
+                    " 3 - Sair");
+            opcao = sc.nextByte();
+            boolean diaValido = false;
+            boolean horaValida = false;
+            boolean mesValido = false;
+            int dia = 0;
+            int hora = 0;
+            int mes = 0;
 
-                if(opcao == 1){ // add compromisso
-                    boolean diaValido = false;
-                    boolean horaValida = false;
-                    int dia = 0;
-                    int hora = 0;
-
-                    while(!diaValido){ // validando o dia
-                    System.out.print("Qual o dia do mês em que você quer adicionar o compromisso? ");
-                    dia = sc.nextInt();
-                    if(dia > 0 && dia<=31){
-                        diaValido = true;
+            if (opcao == 1) { // add compromisso
+                while (!mesValido) { // validando o dia
+                    System.out.print("Qual o mês em que você quer adicionar o compromisso? ");
+                    mes = sc.nextInt();
+                    if (mes > 0 && mes <= 12) {
+                        mesValido = true;
                     } else {
-                        System.out.println("Dia invalido, digite novamente. ");
-                      }
+                        System.out.println("mes invalido, digite novamente. ");
                     }
+                }
 
-                    while(!horaValida){ // validando a hora
-                        System.out.print("Qual o horario do compromisso? ");
-                        hora = sc.nextInt();
-                        if(hora > 0 && hora <= 24){
-                            horaValida = true;
-                        }
-                        else{
-                            System.out.println("Horario invalido, digite novamente.");
-                        }
-                    }
-                    dia --;
-                    System.out.println("Digite o compromisso: ");
-                    compromissos [dia] [hora] = sc.next();
-                    System.out.println("--------------------------------------------");
-
-                } else if(opcao == 2){ // verificar compromisso
-                    boolean diaValido = false;
-                    boolean horaValida = false;
-                    int dia = 0;
-                    int hora = 0;
-
-                    while(!diaValido){ // validando o dia
+                    while (!diaValido) { // validando o dia
                         System.out.print("Qual o dia do mês em que você quer adicionar o compromisso? ");
                         dia = sc.nextInt();
-                        if(dia > 0 && dia<=31){
+                        if (dia > 0 && dia <= 31) {
                             diaValido = true;
                         } else {
                             System.out.println("Dia invalido, digite novamente. ");
                         }
                     }
 
-                    while(!horaValida){ // validando a hora
+                    while (!horaValida) { // validando a hora
                         System.out.print("Qual o horario do compromisso? ");
                         hora = sc.nextInt();
-                        if(hora > 0 && hora <= 24){
+                        if (hora > 0 && hora <= 24) {
                             horaValida = true;
-                        }
-                        else{
+                        } else {
                             System.out.println("Horario invalido, digite novamente.");
                         }
                     }
-                     dia --;
-                    System.out.println("O compromisso agendado é: " + compromissos [dia] [hora] );
+                    mes--;
+                    dia--;
+                    System.out.println("Digite o compromisso: ");
+                    compromissos[mes][dia][hora] = sc.next();
                     System.out.println("--------------------------------------------");
+                } else if (opcao == 2) { // add compromisso
+                    while (!mesValido) { // validando o dia
+                        System.out.print("Qual o mês em que você quer adicionar o compromisso? ");
+                        mes = sc.nextInt();
+                        if (mes > 0 && mes <= 12) {
+                            mesValido = true;
+                        } else {
+                            System.out.println("mes invalido, digite novamente. ");
+                        }
+                    }
 
+                        while (!diaValido) { // validando o dia
+                            System.out.print("Qual o dia do mês em que você quer adicionar o compromisso? ");
+                            dia = sc.nextInt();
+                            if (dia > 0 && dia <= 31) {
+                                diaValido = true;
+                            } else {
+                                System.out.println("Dia invalido, digite novamente. ");
+                            }
+                        }
 
-                } else if (opcao == 0){ // sair do programa
+                        while (!horaValida) { // validando a hora
+                            System.out.print("Qual o horario do compromisso? ");
+                            hora = sc.nextInt();
+                            if (hora > 0 && hora <= 24) {
+                                horaValida = true;
+                            } else {
+                                System.out.println("Horario invalido, digite novamente.");
+                            }
+                        }
+                        mes--;
+                        dia--;
+                    System.out.println("O compromisso agendado é: " + compromissos[mes][dia][hora]);
+                    System.out.println("--------------------------------------------");
+                    }  else if (opcao == 3) { // sair do programa
                     System.out.println("Finalizando programa!!");
                     break;
 
-                } else{
-                    System.out.println("Opção invalida! =( \n"+
-                                                     "          Digite novamente!! =D");
+                } else {
+                    System.out.println("Opção invalida! =( \n" +
+                            "          Digite novamente!! =D");
                 }
             }
-
-
+        }
     }
-}
+
