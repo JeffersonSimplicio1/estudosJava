c79ab178683d997e0722d488cbb9115c5e3b2099
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2025-09-25 22:37:00 -0300

    aprendendo sobre excessões

---
+++ curso_programacao/Excessoes/src/exercicio01.java
@@ -0,0 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class exercicio01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try  {
+            String[] vect = sc.nextLine().split(" ");
+            int posicao = sc.nextInt();
+            System.out.println(vect[posicao]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println(" Posição invalida!!");
+        }
+        catch(InputMismatchException e){
+            System.out.println("Erro de entrada!");
+
+        }
+
+        sc.close();
+    }
+}
